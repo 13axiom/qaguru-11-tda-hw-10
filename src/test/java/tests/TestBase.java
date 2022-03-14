cc6d95e1047a18c6588ab5b284d8629cb4e79b12
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-03-14 14:07:51 +0300

    add selenoid support v4

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -14,7 +14,7 @@ public class TestBase {
 
     @BeforeAll
     static void beforeAll() {
-      //  SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+        //  SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
 
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
