eb6668c6371f5391f0a8b1ac7e05af83f65d80a3
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-03-16 16:01:37 +0300

    experiment new v1

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -22,10 +22,11 @@ public class TestBase {
             Configuration.baseUrl = "https://demoqa.com";
             Configuration.browser = System.getProperty("browser", "chrome");
             Configuration.browserSize = System.getProperty("browser_size", "3840x2160");
+            String remoteBrowserAddress = System.getProperty("remote_browser", "selenoid.autotests.cloud/wd/hub");
+            String remoteBrowserUser = System.getProperty("remote_browser_user");
+            String remoteBrowserPassword = System.getProperty("remote_browser_password");
 
-
-            Configuration.remote = "https://" +   System.getProperty("remote_browser_user") + ":" + System.getProperty("remote_browser_password") + "@" + System.getProperty("remote_browser");
-            System.out.println("u/p " +   Configuration.remote);
+            Configuration.remote = "https://" + remoteBrowserUser + ":" + remoteBrowserPassword + "@" + remoteBrowserAddress;
 
             DesiredCapabilities capabilities = new DesiredCapabilities();
             capabilities.setCapability("enableVNC", true);
