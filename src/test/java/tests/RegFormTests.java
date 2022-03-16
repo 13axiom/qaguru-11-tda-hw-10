c22ab89aa6b307e996e59b3432c2e5d18b701e16
dmitriy.tokarev <dmitriy.tokarev@quadcode.com>
2022-03-16 13:52:25 +0300

    try to add smoke v1

--- src/test/java/tests/RegFormTests.java
+++ src/test/java/tests/RegFormTests.java
@@ -5,9 +5,16 @@ import io.qameta.allure.Feature;
 import io.qameta.allure.Owner;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import pages.RegForm;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.Date;
+import java.util.Locale;
 
 
 public class RegFormTests extends TestBase {
@@ -23,11 +30,12 @@ public class RegFormTests extends TestBase {
             userBdayYear = "1999",
             userSubject = "History",
             userHobby = "Reading",
-           // userPictureName = "cat_4_qaguru_11.jpeg",
-            userCurrAddress = "Main street,90210",
+    // userPictureName = "cat_4_qaguru_11.jpeg",
+    userCurrAddress = "Main street,90210",
             userState = "Uttar Pradesh",
             userCity = "Agra",
             userBdayDay = "30";
+
 
 
     @Test
@@ -36,7 +44,7 @@ public class RegFormTests extends TestBase {
     @Feature("Подключаем Jenkins с Allure report")
     @DisplayName("Результат заполнения формы соответствует введенным значениям в форме")
     void successFillPracticeForm() {
-            regForm.openPage()
+        regForm.openPage()
                 .setFirstName(userName)
                 .setLastName(userSurnname)
                 .setEmail(userEmail)
@@ -64,5 +72,29 @@ public class RegFormTests extends TestBase {
                 //.checkResult("Picture", userPictureName)
                 .checkResult("Address", userCurrAddress)
                 .checkResult("State and City", userState + " " + userCity);
+    }
+
+    @Test
+    @Owner("DmitriyTQC")
+    @Severity(SeverityLevel.NORMAL)
+    @Feature("Тесты формы")
+    @DisplayName("Проверка возможности создать карточку студента с минимальным набором данных")
+    void successMinFillPracticeForm() {
+        regForm.openPage()
+                .setFirstName(userName)
+                .setLastName(userSurnname)
+                .selectMaleGender()
+                .setPhone(userPhone)
+                .clickSubmitBTN();
+
+        String dateNow = regForm.dateNow();
+
+
+        regForm.checkResultHeader()
+                .checkResult("Label", "Values")
+                .checkResult("Student Name", userName + " " + userSurnname)
+                .checkResult("Gender", userGender)
+                .checkResult("Mobile", userPhone)
+                .checkResult("Date of Birth", dateNow);
     }
 }
